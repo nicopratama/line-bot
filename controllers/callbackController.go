8e70f6b30a445d457409b3b097ada0e6f066891e
Nico Pratama <stephanusnicopratama@gmail.com>
2018-02-19 10:07:36 +0700

    tes struct

--- controllers/callbackController.go
+++ controllers/callbackController.go
@@ -3,15 +3,16 @@ package controllers
 import (
 	"log"
 
+	"belajar/bot/models"
+
 	"github.com/gin-gonic/gin"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func Callback(c *gin.Context) {
-	bot, err := linebot.New("06dfbfb76fef562140f6a5d949c45c6a", "4sp/TOv5oBk0NUToWxnYznPn+fY+JnV5Am17sRYPvgymi40sfL8C/kFxxhN6nhUrkaJ+GbBlSvABMeMyYWZpB0mYjke2kFUQZVbyDZK4fiJOeM6yabiOTOCNt+GEzYaHDsDJ4uyMyaxoOvVuK0Ob2QdB04t89/1O/w1cDnyilFU=")
-	if err != nil {
-		log.Fatal(err)
-	}
+	model := model.BotStruct{}
+
+	bot, _ := model.Register()
 
 	events, err := bot.ParseRequest(c.Request)
 	if err != nil {
---
+++ models/bot.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"log"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+type BotStruct struct {
+	ChannelSecret string
+	ChannelToken  string
+}
+
+func (b *BotStruct) Register() (*linebot.Client, error) {
+	b.ChannelSecret = "06dfbfb76fef562140f6a5d949c45c6a"
+	b.ChannelToken = "4sp/TOv5oBk0NUToWxnYznPn+fY+JnV5Am17sRYPvgymi40sfL8C/kFxxhN6nhUrkaJ+GbBlSvABMeMyYWZpB0mYjke2kFUQZVbyDZK4fiJOeM6yabiOTOCNt+GEzYaHDsDJ4uyMyaxoOvVuK0Ob2QdB04t89/1O/w1cDnyilFU="
+	bot, err := linebot.New(b.ChannelSecret, b.ChannelToken)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return bot, nil
+}
