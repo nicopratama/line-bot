d0439df03a774190d9b046e096ad9367583f8247
Nico Pratama <stephanusnicopratama@gmail.com>
2018-03-01 13:45:01 +0700

    tes register

--- handlers/registerHandlers.go
+++ handlers/registerHandlers.go
@@ -4,7 +4,7 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-func RegisterHandler(bot *linebot.Client, replyToken string, source *linebot.EventSource) error {
-	bot.ReplyMessage(replyToken, linebot.NewTextMessage("Anda telah terdaftar"), linebot.NewTextMessage("Halo kakak")).Do()
+func RegisterHandler(bot *linebot.Client, event *linebot.Event) error {
+	bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(event.Source.UserID), linebot.NewTextMessage("Halo kakak")).Do()
 	return nil
 }
