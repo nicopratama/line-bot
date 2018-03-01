1dad656f1fe134e596789fe2e1e594bafdc77a18
Nico Pratama <stephanusnicopratama@gmail.com>
2018-03-01 13:48:44 +0700

    tes

--- handlers/registerHandlers.go
+++ handlers/registerHandlers.go
@@ -2,11 +2,12 @@ package handlers
 
 import (
 	"fmt"
+
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func RegisterHandler(bot *linebot.Client, event *linebot.Event) error {
 	fmt.Println(event.Source.UserID)
-	bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(event.Source.Type), linebot.NewTextMessage("Halo kakak")).Do()
+	bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("<html><head><body><title>Tes</title></body></head></html>"), linebot.NewTextMessage("Halo kakak")).Do()
 	return nil
 }
