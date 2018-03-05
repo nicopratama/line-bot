a61c1407f241d5cae3ca1f3700e19f1e6f2bd3aa
Nico Pratama <stephanusnicopratama@gmail.com>
2018-03-05 16:24:09 +0700

    as

--- services/firestore.go
+++ services/firestore.go
@@ -26,6 +26,7 @@ func RegisterFirestore() *db.Client {
 
 	var tes map[string]interface{}
 	ref := client.NewRef("data")
+	log.Println(ref)
 	if err := ref.Get(ctx, &tes); err != nil {
 		log.Fatalln("Error reading value:", err)
 	}
