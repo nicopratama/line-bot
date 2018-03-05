f7eaf76caa1ad7971a47455015d8030662cfb790
Nico Pratama <stephanusnicopratama@gmail.com>
2018-03-05 14:26:23 +0700

    tes firestore

--- services/firestore.go
+++ services/firestore.go
@@ -5,14 +5,23 @@ import (
 	"log"
 
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
 	"google.golang.org/api/option"
 )
 
-func RegisterFirestore() *firebase.App {
+func RegisterFirestore() *db.Client {
+	ctx := context.Background()
+	conf := &firebase.Config{
+		DatabaseURL: "https://databaseName.firebaseio.com",
+	}
 	opt := option.WithCredentialsFile("./config/line-quiz.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
+	app, err := firebase.NewApp(context.Background(), conf, opt)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return app
+	client, err := app.Database(ctx)
+	if err != nil {
+		log.Fatalln("Error initializing database client:", err)
+	}
+	return client
 }
